2704b6a96cc35d70697a68d33f83c1d1ac8f32be
PavelDemyanenko <mail@paveldemyanenko.com>
2018-11-11 18:38:23 +0300

    Adjust navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
+import NavigationsItems from '../NavigationItems/NavigationItems'
 
 import classes from './Toolbar.css'
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <NavigationsItems />
     <Logo />
     <nav />
   </header>
