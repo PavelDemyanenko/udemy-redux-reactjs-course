5e5b338a30160e9ddb05a04a0e0843563f5bbc5c
PavelDemyanenko <mail@paveldemyanenko.com>
2018-11-18 21:10:24 +0300

    Adjust sidedrawe with backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    )
+  }
+}
 
 export default Layout
